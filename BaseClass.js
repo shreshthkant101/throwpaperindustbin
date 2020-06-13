50fe05871ac5c67be4e6a1fade813173132d660e
shreshthkant101 <63898444+shreshthkant101@users.noreply.github.com>
2020-06-13 16:34:01 +0530

    Update BaseClass.js

--- BaseClass.js
+++ BaseClass.js
@@ -8,13 +8,13 @@ class BaseClass{
         this.body = Bodies.rectangle(x, y, width, height, options);
         this.width = width;
         this.height = height;
-       
+        this.image = loadImage('base.png');
         World.add(world, this.body);
       }
       display(){
         push();
         translate(this.body.position.x, this.body.position.y);
-        
+        image(this.image,0,0,this.width,this.height);
         
         pop();
       }
